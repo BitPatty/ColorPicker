5d0792483dea1f76b88f37cc979ec5475cd2a3c7
Matteias Collet <matteias.collet@bluewin.ch>
2020-12-08 23:03:05 +0100

    fix hex format truncating small values

--- ColorPicker/Helpers.cs
+++ ColorPicker/Helpers.cs
@@ -6,7 +6,7 @@ namespace ColorPicker
   public static class Helpers
   {
     public static string ColorToHexString(Color color)
-      => String.Format("#{0:X}{1:X}{2:X}", color.R, color.G, color.B);
+      => String.Format("#{0:X2}{1:X2}{2:X2}", color.R, color.G, color.B);
 
     public static string ColorToRGBString(Color color)
       => String.Format("{0}, {1}, {2}", color.R, color.G, color.B);
